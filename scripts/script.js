ca04ed07ada0e1cadc74ed195a76f3002b2e898e
Solange Dougan <vanesa.dougan@hotmail.com>
2022-02-24 15:31:47 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('#menu');
+
+let menu_bar = document.querySelector('#menu-bar');
+
+
+menu_bar.addEventListener('click', function()
+
+{
+
+
+menu.classList.toggle ('menu-lateral');
+
+})
